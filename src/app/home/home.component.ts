6e6c2148c3d228ab15400883208a348a1cb2729d
AbdoRayan404 <gamecorner71@gmail.com>
2023-02-01 19:55:50 +0200

    feat: Home page - add Home component through Angular CLI - add default path and home path to app-routing - remove default html in app-component - add router-outlet tags to app-component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  goTo2PGame() {
+    this.router.navigate['/play/twoplayer']
+  }
+}
